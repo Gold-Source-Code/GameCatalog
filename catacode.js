d566e76b990223e40f1ceca41c0fba9d755fc4c4
Gold-Source-Code <maikhermsen1@gmail.com>
2023-06-30 12:46:25 +0200

    THE CODE

---
+++ catacode.js
@@ -0,0 +1,35 @@
+function Game1Desc() {
+    document.getElementById("Game1DescText").innerHTML = "SKATE 3 features an all-new co-op mode where teammates can complete challenges together while advancing each other’s careers. From rewarding epic bails and fails with a beefed up Hall of Meat mode to honing your craft with the all-new skate.School, players of all skill levels have multiple ways to “kill it” in the game. The franchise also continues to raise the bar for user created content with the skate.Create feature suite, an innovative toolset that empowers gamers to truly express their skate style by creating their own graphics, videos, and skate parks."
+}
+
+function Game2Desc() {
+    document.getElementById("Game2DescText").innerHTML = "Dragon Ball Z: Budokai Tenkaichi 3 delivers an extreme 3D fighting experience, improving upon last year's game with over 150 playable characters, enhanced fighting techniques, beautifully refined effects and shading techniques, making each character's effects more realistic, and over 20 battle stages. In addition, an improved control system for the Wii will allow players to easily mimic signature moves and execute devastating energy attacks as they are performed in the Dragon Ball Z animated series."
+}
+
+function Game3Desc() {
+    document.getElementById("Game3DescText").innerHTML = "The game involves players creating and destroying various types of blocks in a three dimensional environment. The player takes an avatar that can destroy or create blocks, forming fantastic structures, creations and artwork across the various multiplayer servers in multiple game modes."
+}
+
+function Game1Title(){
+    document.getElementById("Game1TitleText").innerHTML = "Skate 3"
+}
+
+function Game2Title(){
+    document.getElementById("Game2TitleText").innerHTML = "Budokai Tenkaichi 3"
+}
+
+function Game3Title(){
+    document.getElementById("Game3TitleText").innerHTML = "Minecraft"
+}
+
+function Game1Requirements(){
+    document.getElementById("Game1RequireText").innerHTML = "Requirements: <br> <br> Operating System: Windows 7 and the KB3135445 platform update (64-bit) <br> <br> Processor: AMD Vishera FX-6350 3.9GHz or higher; Intel Pentium Dual-Core G4400 3.30GHz or higher <br> <br> Ram: 4 GB or more <br> <br> Free Hard Drive Space: 14 GB or more <br> <br> Graphics Card: ASUS Radeon R7 250; NVIDIA GeForce GTX 750 or higher <br> <br> DirectX: Version 11"
+}
+
+function Game2Requirements(){
+    document.getElementById("Game2RequireText").innerHTML = "Requirements: <br> <br> Operating System: Windows XP/Vista/7/8/8.1/10 <br> <br> Processor: INTEL 3.0 GHz Dual Core <br> <br> Ram: 2 GB or more <br> <br> Free Hard Drive Space: 1 GB or more <br> <br> Graphics Card: ASUS Radeon R7 250; NVIDIA GeForce GTX 750 or higher <br> <br> DirectX: Version 9"
+}
+
+function Game3Requirements(){
+    document.getElementById("Game3RequireText").innerHTML = "Requirements: <br> <br> Operating System: Windows 7 (64-bit) <br> <br> Processor: Intel Core i3 3210 | AMD A8 7600 APU or equivalent <br> <br> Ram: 4 GB or more <br> <br> Free Hard Drive Space: 1 GB or more <br> <br> Graphics Card: Intel HD Graphics 4000 or AMD Radeon R5 series | NVIDIA GeForce 400 Series or AMD Radeon HD 7000 series <br> <br> DirectX: Version 11"
+}
